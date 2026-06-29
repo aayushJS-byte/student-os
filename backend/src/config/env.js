0a251492f355d00c287905b772f2ef42d79c1476
aayushJS-byte <aayuxgupta123@gmail.com>
2026-06-29 23:24:15 +0530

    chore: bootstrap backend architecture

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const env = {
-    PORT: process.env.PORT || 5000,
+    PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV,
 
     MONGODB_URI: process.env.MONGODB_URI,
